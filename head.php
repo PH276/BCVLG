51e077146730b5ea65c7eec5469f8541a93fd489
PH276 <pascal.huitorel@lepoles.com>
2017-09-20 00:35:19 +0200

    init 2

---
+++ head.php
@@ -0,0 +1,20 @@
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- ****************************** icone ****************************** -->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+
+    <!-- ********************** css Bootstrap ****************************** -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!-- <script src="https://use.fontawesome.com/f29720d172.js"></script> -->
+
+    <link rel="stylesheet" href="css/style.css">
+
+
+
+    <title><?php echo $titrePage.'Billard Club Villeneuve-la-Garenne' ?></title>
+</head>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+$titrePage = "";
+include ('head.php');
+?>
+<body>
+    <?php include ('header.php'); ?>
+
+
+    <?php include ('footer.php'); ?>
+</body>
+</html>
