ad2e00d922480e32303cffb93a2a397375e90379
PH276 <pascal.huitorel@lepoles.com>
2017-10-01 01:27:35 +0200

    header

---
+++ inc/header.inc.php
@@ -0,0 +1,15 @@
+<header class="container-fluid">
+    <div class="row">
+        <div class="col-6">
+
+            <p>Le Billard Club de<br>Villeneuve-la-Garenne</p>
+            <p>vous accueille du Lundi au Dimanche de 15h00 à 20h00</p>
+            <ul>
+                <li>5 billards français, 1 billard américain. </li>
+                <li>Une ambiance amicale.</li>
+                <li>Des compétitions de Novembre à Juin. </li>
+            </ul>
+        </div>
+        <img class="col-6" src="img/billes.png" alt="">
+    </div>
+</header>
