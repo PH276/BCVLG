f9c88f6917b70a249bc2183cf4cbe6079eea2ab1
PH276 <pascal.huitorel@gmail.com>
2020-03-31 15:07:50 +0200

    modification de la page contact

--- contact.php
+++ contact.php
@@ -48,7 +48,9 @@ include ('inc/head.inc.php');
                                 <hr color="#99CC00" size="5">
                                 <div class="row">
                                     <div class="col-12">
-                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.911088195635!2d2.3294437159127517!3d48.936135279295264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66ed4deb5e9fd%3A0xf3f6f20d3ea68888!2s13+Rue+Dupont+du+Chambon%2C+92390+Villeneuve-la-Garenne!5e0!3m2!1sfr!2sfr!4v1506638531293" width="80%" frameborder="0" style="border:0" allowfullscreen></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d965.3343924239839!2d2.331265732434719!3d48.936116437114876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66ed4deb46db7%3A0xb07a07db86e90400!2sBillard+Club+VIG!5e1!3m2!1sfr!2sfr!4v1560975788547!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!4v1560976654332!6m8!1m7!1su2fcYbHbFf3eeLMeol66aA!2m2!1d48.93607495650893!2d2.331753016868828!3f290.8103485767137!4f13.603846610007636!5f0.4000000000000002" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!4v1560976729993!6m8!1m7!1su2fcYbHbFf3eeLMeol66aA!2m2!1d48.93607495650893!2d2.331753016868828!3f276.3268298989461!4f-1.5538839435424308!5f3.325193203789971" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                                     </div>
                                 </div>
                             </section>
@@ -59,7 +61,6 @@ include ('inc/head.inc.php');
 
                 <?php include ('inc/footer.inc.php'); ?>
             </div>
-
 
         </body>
         </html>
